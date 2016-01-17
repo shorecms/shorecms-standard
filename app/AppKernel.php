a72241b3e6338a18ed8aebda0db4043e3fb762f6
Richard Eyre <richard@teabagstudios.com>
2016-01-17 12:40:53 +0000

    Added new CoreBundle and UserBundle from shorecms

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new ShoreCms\Bundle\CoreBundle\CoreBundle(),
+            new ShoreCms\Bundle\UserBundle\UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
